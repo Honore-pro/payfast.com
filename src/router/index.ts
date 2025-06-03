e156962c0ad88e3c84aa1e564a900f050cc08ea2
Honore-pro <cyizairakoze9@gmail.com>
2025-06-03 14:27:16 +0200

    open again

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from '../views/HomeView.vue',
-import Skills from '../views/skillsView.vue',
-import Projects from '../views/ProjectView.vue',
-import About from '../views/AboutView.vue'
+import Home from '../views/HomeView.vue';
+import Skills from '../views/skillsView.vue';
+import Projects from '../views/ProjectView.vue';
+import About from '../views/AboutView.vue';
 
 const routes = [
   { path: '/', component: Home },
